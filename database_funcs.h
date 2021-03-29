4ab83b45e285ddb015acd7628ddeb6ed9a6a51c1
Xueyang Liu <xl350@duke.edu>
2021-03-29 06:21:21 -0400

    modified database and init table funcs

--- database_funcs.h
+++ database_funcs.h
@@ -16,9 +16,12 @@ void disconnectDB(connection *C);
 void executeQuery(string query, connection *C);
 void dropTable(const char *fileName, connection *C);
 void createTable(const char *fileName, connection *C);
+// if the account exist
+bool accountExist();
 //create an account
 void createAccount(long account_id, double balance);
 //create or add <share> shares of <symbol_name> into account <account_id>
 void addPosition(string symbol_name, long account_id, int share);
 
+void queryTrans(long trans_id, long account_id);
 #endif
