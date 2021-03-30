d7e869289819506f1e9ab1424e34bace746301e4
CalesChen <holyking98328@gmail.com>
2021-03-30 02:41:54 -0400

    Head File Done

--- database_funcs.h
+++ database_funcs.h
@@ -28,4 +28,11 @@ void createSymbol(connection *C, string symbol_name);
 bool addPosition(connection *C,string symbol_name, long account_id, double share);
 bool transactionExist(connection *C, long trans_id);
 string queryTrans(connection *C,long trans_id, long account_id);
+
+// Match Order Related Function
+bool matchOrderBuyer(connection *C, long buyer_tran_id);
+bool matchOrderSeller(connection *C, long seller_tran_id);
+void updateTransactionStatus(work & W, long open_trans_id, long close_trans_id, double amountRemain, long buyer_id, long seller_id, string & sym, double shares, double balance);
+void updateTransactionStatus(work & W, long trans_id, double amount, string status);
+void updateAccountShareAndMoney(work & W, long buyer_id, long seller_id, string & sym, double shares, double balance);
 #endif
