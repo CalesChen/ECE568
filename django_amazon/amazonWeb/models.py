0e46c254b681b7764e5b60a3c89fc037132f4738
Xueyang Liu <xl350@duke.edu>
2021-04-14 10:59:25 -0400

    add product, package, order tables

--- django_amazon/amazonWeb/models.py
+++ django_amazon/amazonWeb/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
-class Categories(models.Model):
+class Category(models.Model):
     category = models.CharField(max_length=20, null = False, blank = False)
 
     def __str__(self):
@@ -15,3 +15,34 @@ class Warehouse(models.Model):
 
     def __str__(self):
         return f'({self.address_x}, {self.address_y})'
+
+class Product(models.Model):
+    # product id using default serial int as primary key
+    description = models.CharField(max_length=200, null = False, blank = False)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.SET_NULL, null = True)
+    price = models.FloatField(default = 9.99, null = False, blank = False)
+
+    def __str__(self):
+        return f'<{self.description}, {self.price}>'
+
+class Package(models.Model):
+    customer = models.ForeignKey(User, related_name='packages', on_delete=models.CASCADE)
+    warehouse = models.ForeignKey(Warehouse, related_name='packages', on_delete=models.SET_NULL,null = True)
+    address_x = models.IntegerField(default=1)
+    address_y = models.IntegerField(default=1)
+    pack_time = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=50,default = "in process")
+    ups_username = models.CharField(max_length=16,default = "", blank = True)
+
+    def __str__(self):
+        return f"{self.customer}'s package at {self.warehouse}, {self.status}"
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='orders', on_delete=models.SET_NULL, null = True)
+    product_num = models.IntegerField(default = 1)
+    package = models.ForeignKey(Package, related_name='orders', on_delete=models.SET_NULL, null = True)
+    generate_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.product_num} {self.product} in {self.package}'
